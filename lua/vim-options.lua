3e7a87c8605d2198e2dd66e11607a76ac075fd9c
Dicha <dimitrichauvinhameau@gmail.com>
2024-07-05 17:24:44 +0200

    config lua perso

---
+++ lua/vim-options.lua
@@ -0,0 +1,12 @@
+-- Set up tabs and spaces
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+-- Set leader keys
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Set number line for files 
+vim.cmd("set number")
