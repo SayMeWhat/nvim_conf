17d07ece8b7f84913145f4c833f09fa314c59f3a
Dicha <dimitrichauvinhameau@gmail.com>
2024-07-10 20:45:33 +0200

    change key leader to \ from space

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,7 +5,7 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
 -- Set leader keys
-vim.g.mapleader = " "
+vim.g.mapleader = "\\"
 vim.g.maplocalleader = "\\"
 
 -- Set number line for files 
