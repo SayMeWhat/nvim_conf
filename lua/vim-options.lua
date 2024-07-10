be6f2320b89bb681f487e1d10abd7b55a121b6c4
Dicha <dimitrichauvinhameau@gmail.com>
2024-07-10 12:27:43 +0200

    add config for use toggleterm with windows or linux

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -12,4 +12,11 @@ vim.g.maplocalleader = "\\"
 vim.cmd("set number")
 
 -- Set  global  terminal 
-vim.o.shell = "C:\\Program Files\\PowerShell\\7\\pwsh.exe"
+local os_name =  vim.loop.os_uname().sysname
+
+if os_name == "Windows_NT"  then
+  vim.o.shell = 'C:\\Program Files\\PowerShell\\7\\pwsh.exe'
+elseif os_name == 'Linux' then
+  vim.o.shell  =  '/bin/zsh'
+else  vim.o.shell = '/bin/sh'
+end
