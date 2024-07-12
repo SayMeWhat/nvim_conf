fc1ca66869b4e0cf8558518ab1689b73aad44c50
Dicha <dimitrichauvinhameau@gmail.com>
2024-07-12 17:29:50 +0200

    add leetcode plugin

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -24,3 +24,8 @@ end
 
 -- Set clipboard
 vim.cmd("set clipboard=unnamedplus")
+
+
+-- Set copy/past cmd
+vim.keymap.set({ "n", "x" }, "<leader>y", [["+y]]) -- copy to system clipboard
+vim.keymap.set({ "n", "x" }, "<leader>p", [["+p]]) -- paste from system clipboard
