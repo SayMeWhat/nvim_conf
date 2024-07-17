70a177b73f486168034e4b172d7ba595aeeb8672
Dicha <dimitrichauvinhameau@gmail.com>
2024-07-17 16:16:42 +0200

    format code

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,31 +1,30 @@
 -- Set up tabs and spaces
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.cmd 'set expandtab'
+vim.cmd 'set tabstop=2'
+vim.cmd 'set softtabstop=2'
+vim.cmd 'set shiftwidth=2'
 
 -- Set leader keys
-vim.g.mapleader = "\\"
-vim.g.maplocalleader = "\\"
+vim.g.mapleader = '\\'
+vim.g.maplocalleader = '\\'
 
--- Set number line for files 
-vim.cmd("set relativenumber")
+-- Set number line for files
+vim.cmd 'set relativenumber'
 
--- Set  global  terminal 
-local os_name =  vim.loop.os_uname().sysname
+-- Set  global  terminal
+local os_name = vim.loop.os_uname().sysname
 
-if os_name == "Windows_NT"  then
+if os_name == 'Windows_NT' then
   vim.o.shell = 'C:\\Program Files\\PowerShell\\7\\pwsh.exe'
 elseif os_name == 'Linux' then
-  vim.o.shell  =  '/bin/zsh'
-else  vim.o.shell = '/bin/sh'
+  vim.o.shell = '/bin/zsh'
+else
+  vim.o.shell = '/bin/sh'
 end
 
-
 -- Set clipboard
-vim.cmd("set clipboard=unnamedplus")
-
+vim.cmd 'set clipboard=unnamedplus'
 
 -- Set copy/past cmd
-vim.keymap.set({ "n", "x" }, "<leader>y", [["+y]]) -- copy to system clipboard
-vim.keymap.set({ "n", "x" }, "<leader>p", [["+p]]) -- paste from system clipboard
+vim.keymap.set({ 'n', 'x' }, '<leader>y', [["+y]]) -- copy to system clipboard
+vim.keymap.set({ 'n', 'x' }, '<leader>p', [["+p]]) -- paste from system clipboard
